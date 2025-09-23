0ab73390eebe8c01a70d2b063877b9b8029facfc
semsemin <semin0409@naver.com>
2025-09-24 00:35:04 +0900

    feat:#2- 서비스별 모듈 정리

---
+++ shop-service/src/test/java/ShopServiceApplicationTests.java
@@ -0,0 +1,9 @@
+//import org.junit.jupiter.api.Test;
+//
+//@SpringBootTest
+//@ContextConfiguration(classes = ShopTestConfiguration.class)
+//class ShopServiceApplicationTests {
+//    @Test
+//    void contextLoads() {
+//    }
+//}
