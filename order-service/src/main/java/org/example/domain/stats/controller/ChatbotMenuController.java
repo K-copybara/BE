31550668439ec6e4e9882b3846a9da46e6786dc5
semsemin <semin0409@naver.com>
2025-10-08 17:11:07 +0900

    feat: #28 - 특정 메뉴 판매량

--- order-service/src/main/java/org/example/domain/stats/controller/ChatbotMenuController.java
+++ order-service/src/main/java/org/example/domain/stats/controller/ChatbotMenuController.java
@@ -1,6 +1,7 @@
 package org.example.domain.stats.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.domain.stats.dto.request.MenuSalesRequest;
 import org.example.domain.stats.dto.request.Top3MenuRequest;
 import org.example.domain.stats.service.MenuStatsService;
 import org.example.dto.Response;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/customer")
@@ -18,11 +21,21 @@ public class ChatbotMenuController {
 
     private final MenuStatsService menuStatsService;
 
+    // 상위 3개
     @PostMapping("/top3-menus")
     public ResponseEntity<Response<Top3MenuResponseEvent>> getTop3Menus(
             @RequestBody Top3MenuRequest request
     ) throws Exception {
         Top3MenuResponseEvent event = menuStatsService.getTop3Menus(request.getStoreId());
         return ResponseEntity.ok(Response.success("한 달간 판매량 상위 3개 메뉴 조회 성공", event));
+    }
+
+    // 특정 메뉴 판매량
+    @PostMapping("/menu-sales")
+    public ResponseEntity<Response<Map<Long, Long>>> getMenuSales(
+            @RequestBody MenuSalesRequest request
+    ) {
+        Map<Long, Long> result = menuStatsService.getMonthlySales(request.getMenuIds());
+        return ResponseEntity.ok(Response.success("한 달간 메뉴 판매량 조회 성공", result));
     }
 }
