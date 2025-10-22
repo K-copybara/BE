6c45792d97776c2bbcba061d13b946bc2ee9e2b9
semsemin <semin0409@naver.com>
2025-10-23 02:05:49 +0900

    Revert "Update MerchantOrderController.java"
    This reverts commit 30e2cfa8905e1de655dd39879678646ddd904f28.

--- order-service/src/main/java/org/example/domain/order/controller/MerchantOrderController.java
+++ order-service/src/main/java/org/example/domain/order/controller/MerchantOrderController.java
@@ -19,7 +19,7 @@ public class MerchantOrderController {
     // 사장 주문 내역 조회
     @GetMapping
     public Response<List<MerchantOrderSummaryDto>> getOrdersByStatus(
-            @RequestHeader("X-Store-Id") Long storeId,
+            @RequestParam Long storeId,
             @RequestParam(required = false) String status
     ) {
         OrderStatus orderStatus = (status != null)
